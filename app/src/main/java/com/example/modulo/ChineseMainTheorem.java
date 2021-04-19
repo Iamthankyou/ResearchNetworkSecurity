a746066c670bbc4fc250350a42978fc2ab670535
Le Quang Duy <quangduynb00@gmail.com>
2021-04-19 13:07:51 +0700

    Fail

---
+++ app/src/main/java/com/example/modulo/ChineseMainTheorem.java
@@ -0,0 +1,14 @@
+package com.example.modulo;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class ChineseMainTheorem extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_chinese_main_theorem);
+    }
+}
---
+++ app/src/main/java/com/example/modulo/FenceRail.java
@@ -0,0 +1,14 @@
+package com.example.modulo;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class FenceRail extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_fence_rail);
+    }
+}
